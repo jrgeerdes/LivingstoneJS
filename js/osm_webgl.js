9d86a578cc8db0f4bc75380d269474f9386c396c
Jeremy Geerdes <jrgeerdes@gmail.com>
2014-08-13 23:24:16 -0500

    Tiles, Markers, and Scrolling, oh my

--- js/osm_webgl.js
+++ js/osm_webgl.js
@@ -35,6 +35,11 @@ minimize the code.
         if(opt_options['viewport']){
             this['setViewport'](opt_options['viewport']);
         }
+        
+        // load listener
+        if(this.map_loaded_listener){
+            this.map_loaded_listener();
+        }
     }
     make_public('Map', Map); // export to the public namespace
     Map.prototype.init_env = function(){
@@ -51,10 +56,15 @@ minimize the code.
         } else { // if webgl is unavailable, let's bounce to 2d canvas stuff
             this.use_webgl = 0;
             context = map_canvas.getContext('2d');
-            context.fillStyle = 'rgba(100, 100, 100, 0.5)';
+            context.fillStyle = 'rgba(100, 100, 100, 1)';
             context.fillRect(0, 0, map_canvas.width, map_canvas.height);
         }
         this.context = context;
+        
+        // set up the event listeners
+        add_dom_event('mousedown', create_method_closure(this, Map.prototype.start_drag), map_canvas);
+        add_dom_event('mouseup', create_method_closure(this, Map.prototype.end_drag)); // we attach the mouseup to the window, in case someone drags off the map and releases there
+//        add_dom_event('click', create_method_closure(this, Map.prototype.click), map_canvas);
     };
     Map.prototype['setMapType'] = function(map_type){
         if(Array.isArray(map_type)){
@@ -66,10 +76,34 @@ minimize the code.
                 this.map_types = [new map_type(this)]
         }
         this.render();
+        if(this.map_type_change_listener){
+            this.map_type_change_listener();
+        }
     };
-    Map.prototype['setViewport'] = function(viewport){
-        this.viewport = viewport;
+    Map.prototype['setViewport'] = function(new_viewport){
+        var viewport = this.viewport = this.viewport || {},
+        opt_options = this.options,
+        max_zoom = opt_options['max_zoom'] || 19,
+        min_zoom = opt_options['min_zoom'] || 0;
+        for(var i in new_viewport){
+            if(i == 'zoom'){
+                new_viewport[i] = Math.max(new_viewport[i], min_zoom);
+                new_viewport[i] = Math.min(new_viewport[i], max_zoom);
+            }
+            viewport[i] = new_viewport[i];
+        }
         this.render();
+        if(this.viewport_change_listener){
+            this.viewport_change_listener();
+        }
+    };
+    Map.prototype.get_center_as_pt = function(){
+        var canvas = this.map_canvas,
+        center_pt = new Point(
+            Math.round(canvas.width / 2) + this.offsetLeft,
+            Math.round(canvas.height / 2) + this.offsetTop
+        );
+        return center_pt;
     };
     Map.prototype.render = function(){
         // calculate the offset of the top-left corner of the map
@@ -79,10 +113,11 @@ minimize the code.
         viewport = this.viewport,
         map_canvas = this.map_canvas;
         if(viewport && this.map_types && this.map_types.length > 0){
-            if(viewport['center']){
-                var center_point = map_type['fromLatLngToPoint'](viewport['center']);
-                offsetLeft = this.offsetLeft = Math.max(0, center_point.x - Math.round(map_canvas.width / 2));
-                offsetTop = this.offsetTop = Math.max(0, center_point.y - Math.round(map_canvas.height / 2));
+            if(viewport['center'] && typeof(viewport['zoom']) == 'number'){ // if we have a center and zoom
+                var center_point = viewport['center'] instanceof Point ? viewport['center'] : map_type['fromLatLngToPoint'](viewport['center']);
+                offsetLeft = this.offsetLeft = (center_point.x - Math.round(map_canvas.width / 2));
+                offsetTop = this.offsetTop = (center_point.y - Math.round(map_canvas.height / 2));
+            } else if(viewport['bounds']){ // if we have LatLngBounds
             }
                     
             // render the tiles
@@ -90,9 +125,9 @@ minimize the code.
                 var map_type = this.map_types[i],
                 tileWidth = map_type.options['width'] || 256,
                 tileHeight = map_type.options['height'] || 256,
-                x = Math.floor(offsetLeft / tileWidth),
-                y = Math.floor(offsetTop / tileHeight);
-                while(x * tileWidth < offsetLeft + map_canvas.width){
+                x = Math.floor(offsetLeft / tileWidth); // this line needs to be corrected to allow for spanning backward past 0
+                while(x * tileWidth < map_canvas.width + offsetLeft){
+                    var y = Math.floor(offsetTop / tileHeight); // this line needs to be corrected to allow for spanning backward past 0
                     while(y * tileHeight < offsetTop + map_canvas.height){
                         map_type.placeTile(x, y, (this.viewport['zoom'] || 0));
                         y++;
@@ -102,14 +137,103 @@ minimize the code.
             }
         
             // render overlays
-
+            var overlays = this.overlays;
+            for(var i=0; overlays && i < overlays.length; i++){
+                var overlay = overlays[i];
+                overlay.draw();
+            }
+            
             // render markers
         
             // render infowindows
+
+            if(this.map_render_listener){
+                this.map_render_listener();
+            }
         }
     };
     Map.prototype['getZoom'] = function(){
         return this.viewport['zoom'];
+    };
+    Map.prototype['zoomOut'] = function(){
+        this['setViewport']({
+            'zoom' : Math.max(0, this.viewport['zoom'] - 1)
+        });
+    };
+    Map.prototype['zoomIn'] = function(){
+        this['setViewport']({
+            'zoom' : Math.max(0, this.viewport['zoom'] + 1)
+        });
+    };
+    Map.prototype.addOverlay = function(overlay, dont_render){
+        this.removeOverlay(overlay, 1)
+        var overlays = this.overlays = this.overlays || [];
+        overlays.push(overlay);
+        if(!dont_render){
+            this.render();
+        }
+        
+        if(this.overlay_added_listener){
+            this.overlay_added_listener();
+        }
+    };
+    Map.prototype.removeOverlay = function(overlay, dont_render){
+        var overlays = this.overlays;
+        for(var i=0; overlays && i < overlays.length; i++){
+            var test_overlay = overlays[i];
+            if(test_overlay === overlay){
+                overlays.splice(i, 1);
+            }
+        }
+        if(!dont_render){
+            this.render();
+        }
+        
+        if(this.overlay_removed_listener){
+            this.overlay_removed_listener();
+        }
+    };
+    Map.prototype.start_drag = function(e){
+        this.mousemove_listener = add_dom_event('mousemove', create_method_closure(this, Map.prototype.drag, [{
+            e : e,
+            center : this.get_center_as_pt()
+        }]), window);
+
+        if(this.drag_start_listener){
+            this.drag_start_listener();
+        }
+    };
+    Map.prototype.end_drag = function(e){
+        remove_dom_event(this.mousemove_listener);
+
+        if(this.drag_end_listener){
+            this.drag_end_listener();
+        }
+    };
+    Map.prototype.click = function(e){
+        
+    };
+    Map.prototype.drag = function(anchor, e){
+        var x_moved = anchor.e.clientX - e.clientX,
+        y_moved = anchor.e.clientY - e.clientY,
+        new_center_pt = new Point(
+            anchor.center.x + x_moved,
+            anchor.center.y + y_moved
+        );
+/* debugging only
+        if(console && console.log){
+            console.log(anchor);
+            console.log(e);
+            console.log(this.offsetLeft + ' | ' + this.offsetTop);
+        }
+ debugging only */
+        this.setViewport({
+            'center' : this.map_types[0]['fromPointToLatLng'](new_center_pt)
+        });
+        
+        if(this.drag_listener){
+            this.drag_listener();
+        }
     };
     
     
@@ -119,6 +243,13 @@ minimize the code.
         this.x = x;
         this.y = y;
     }
+    Point.prototype['x'] = function(){
+        return this.x;
+    }
+    Point.prototype['y'] = function(){
+        return this.y;
+    }
+    make_public('Point', Point);
     
     
     
@@ -128,13 +259,13 @@ minimize the code.
     MapType.prototype['fromLatLngToPoint'] = function(pt){
         var lat = pt.lat,
         lng = pt.lng,
-        pi = Math.pi(),
+        pi = Math.PI,
         zoom = this.map['getZoom'](),
         e = Math.sin(lat * pi / 180);
         e = Math.max(e, -.9999);
         e = Math.min(e, .9999);
 
-        var y = Math.round((256 * (Math.pow(2, zoom - 1))) + (.5 * Math.log((1 + e) / (1 - e)) * ((-256 * Math.pow(2, zoom)) / (2 * Math.pi())))),
+        var y = Math.round((256 * (Math.pow(2, zoom - 1))) + (.5 * Math.log((1 + e) / (1 - e)) * ((-256 * Math.pow(2, zoom)) / (2 * Math.PI)))),
         x = Math.round((256 * Math.pow(2, zoom - 1)) + (lng * ((256 * Math.pow(2, zoom)) / 360)));
         
         return new Point(x, y);
@@ -146,10 +277,10 @@ minimize the code.
         tileHeight = this.options['tileHeight'] || 256,
         tileWidth = this.options['tileWidth'] || 256,
         zoom = this.map['getZoom'](),
-        pi = Math.pi(),
-        lng = (x - Math(2, zoom - 1)) / (Math(2, zoom) / 360),
-        e = (y - Math(2, zoom - 1)) / (-(Math.pow(2, zoom)) / (2 * pi)),
-        lat = ((2 * Math.atan(Math.log(e))) - (pi / 2)) / (pi / 180);
+        pi = Math.PI,
+        lng = (x - (256 * Math.pow(2, zoom - 1))) / (256 * Math.pow(2, zoom) / 360),
+        e = (y - (256 * Math.pow(2, zoom - 1))) / (-256 * (Math.pow(2, zoom)) / (2 * pi)),
+        lat = ((2 * Math.atan(Math.exp(e))) - (pi / 2)) / (pi / 180);
         
         lat = Math.max(lat, -90);
         lat = Math.min(lat, 90);
@@ -180,21 +311,30 @@ minimize the code.
         var img = tiles[zoom][x][y] = new Image;
         img.onload = create_method_closure(this, function(x, y, zoom){
             this.tiles[zoom][x][y].osm_webgl_ready = 1; // let's assign a property to verify that the tile is loaded
-            this.placeTile(x, y, zoom);
+            this.map.render();
+//            this.placeTile(x, y, zoom);
         }, [x, y, zoom]);
         img.src = this['resolveTileUrl'](x, y, zoom);
         
         tiles[zoom][x][y] = img;
     };
     MapType.prototype.placeTile = function(x, y, zoom){
+//        console && console.log && console.log(([x, y, zoom]).join(' | ')); // debugging only
         var tiles = this.tiles = this.tiles || {},
-        x = x % Math.pow(2, zoom),
-        y = y % Math.pow(2, zoom),
-        img = tiles[zoom] && tiles[zoom][x]? tiles[zoom][x][y] : undefined,
+        total_tiles = Math.pow(2, zoom),
+        tileX = x % total_tiles, // the actual x coord of tile we're going to draw
+        tileY = y % total_tiles, // the actual y coord of tile we're going to draw
         map = this.map,
         opt_options = this.options,
         tileWidth = opt_options['width'] || 256,
         tileHeight = opt_options['height'] || 256;
+        if(tileX < 0){
+            tileX = total_tiles + tileX;
+        }
+        if(tileY < 0){
+            tileY = total_tiles + tileY;
+        }
+        var img = tiles[zoom] && tiles[zoom][tileX]? tiles[zoom][tileX][tileY] : undefined;
         if(img && img.osm_webgl_ready){
             var imgX = (x * tileWidth) - map.offsetLeft,
             imgY = (y * tileHeight) - map.offsetTop;
@@ -202,12 +342,15 @@ minimize the code.
                 map.context.drawImage(img, imgX, imgY);
             }
         } else if(!img){
-            this.generateTile(x, y, zoom);
+            this.generateTile(tileX, tileY, zoom);
         }
     };
     MapType.STREET_MAP = function(map, opt_options){
         this.map = map;
-        this.options = opt_options || {};
+        this.options = opt_options || {
+            'max_zoom' : 19,
+            'min_zoom' : 0
+        };
     }
     extend_class(MapType.STREET_MAP, MapType);
     
@@ -215,7 +358,108 @@ minimize the code.
     
     
     function Marker(opt_options){
+        this['setOptions'](opt_options);
     }
+    Marker.prototype.draw = function(){
+        if(this.options['map']){
+            var opt_options = this.options,
+            map = opt_options['map'],
+            context = map.context,
+            position = map.map_types[0]['fromLatLngToPoint'](opt_options['position']);
+// TODO: add support for tiling, in case the same location is displayed more than once
+            if(opt_options['icon']){ // if we have an icon given, we'll need to get it and draw it in the correct place
+                var img = this.img
+                if(img){ // if we have the image already
+                    var anchor = new Point( // get the anchoring point, which is relative to the upper-left corner of the marker. If we don't have one, we'll anchor it the center of the bottom of the image
+                        opt_options['anchor'] ? opt_options['anchor'].x : img.height,
+                        opt_options['anchor'] ? opt_options['anchor'].y : Math.round(img.width / 2)
+                    ),
+                    imgX = position.x - anchor.x - map.offsetLeft,
+                    imgY = position.y - anchor.y - map.offsetTop;
+                    context.drawImage(img, imgX, imgY);
+                } else {
+                    img = this.img = new Image;
+                    img.src = opt_options['icon'];
+                    img.onload = create_method_closure(this, Marker.prototype.draw);
+                }
+            } else { // if we're drawing a standard marker, let's do it.
+                var startingPos = new Point(position.x - 3 - map.offsetLeft, position.y - 30 - map.offsetTop),
+                anchorPos = new Point(position.x - map.offsetLeft, position.y - map.offsetTop),
+                pole_width = 6,
+                pole_half = Math.round(pole_width / 2),
+                flag_height = 24,
+                txt_width = opt_options['label'] ? context.measureText(opt_options['label']).width : 0,
+                flag_width = Math.max(32, txt_width + 10),
+                flag_x = startingPos.x + pole_half + 1,
+                flag_perspective_difference = Math.round(pole_half * .75);
+                context.lineWidth = 1;
+                if(opt_options['label_font']){
+                    context.font = opt_options['label_font'] || '14px Arial';
+                }
+
+                // draw the flag
+                context.fillStyle = opt_options['color'] || 'rgba(255, 75, 75, 1)';
+                context.beginPath();
+                context.moveTo(startingPos.x + pole_width, startingPos.y);
+                context.lineTo(startingPos.x + pole_width + flag_width, startingPos.y);
+                context.lineTo(startingPos.x + pole_width + flag_width - flag_perspective_difference, startingPos.y + flag_height);
+                context.lineTo(startingPos.x + pole_width - flag_perspective_difference, startingPos.y + flag_height);
+                context.fill();
+
+                // draw the flagpole
+                for(var i=0; i < pole_width; i++){
+                    var rgb_val = 175 - (50 * Math.max(i - pole_half, 0));
+                    context.beginPath();
+                    context.moveTo(startingPos.x + i, startingPos.y);
+                    context.lineTo(anchorPos.x, anchorPos.y);
+                    context.strokeStyle = 'rgba(' + rgb_val + ', ' + rgb_val + ', ' + rgb_val + ', 1)';
+                    context.stroke();
+                }
+                
+                // draw the cap of the flagpole
+                context.beginPath();
+                context.moveTo(startingPos.x, startingPos.y);
+                context.bezierCurveTo(startingPos.x, startingPos.y - 2, startingPos.x + pole_width, startingPos.y - 2, startingPos.x + pole_width, startingPos.y);
+                context.bezierCurveTo(startingPos.x + pole_width, startingPos.y + 2, startingPos.x, startingPos.y + 2, startingPos.x, startingPos.y);
+                context.fillStyle = 'rgba(175, 175, 175, 1)';
+                context.fill();
+                
+                // draw the label, if any
+                if(opt_options['label']){
+                    var txt_x = Math.round((flag_width - txt_width) / 2) - Math.round(flag_perspective_difference * .75),
+                    txt_height = this.txt_height;
+                    if(typeof(txt_height) == 'undefined'){
+                        var txt_height_el = create_element('div', [document.createTextNode(opt_options['label'])], null, {
+                            'font' : opt_options['label_font'] || '14px Arial',
+                            'position' : 'absolute',
+                            'visibility' : 'hidden'
+                        });
+                        document.body.appendChild(txt_height_el);
+                        txt_height = this.txt_height = txt_height_el.offsetHeight;
+                        document.body.removeChild(txt_height_el);
+                    }
+                    context.fillStyle = opt_options['label_color'] || '#000';
+                    context.fillText(
+                        opt_options['label'],
+                        startingPos.x + pole_width + txt_x,
+                        startingPos.y + flag_height - Math.round((flag_height - (txt_height * .66)) / 2)
+                    );
+                }
+            }
+        }
+    };
+    Marker.prototype['setOptions'] = function(opt_options){
+        this.options = {}; // default options
+        for(var i in opt_options){
+            this.options[i] = opt_options[i];
+        }
+        var map = opt_options['map'];
+        if(map){
+            map.addOverlay(this, 1);
+            this.draw();
+        }
+        this.txt_height = undefined;
+    };
     make_public('Marker', Marker);
     
     
@@ -259,8 +503,20 @@ minimize the code.
         this.lat = lat;
         this.lng = lng;
     }
+    LatLng.prototype['lat'] = function(){
+        return this.lat;
+    }
+    LatLng.prototype['lng'] = function(){
+        return this.lng;
+    }
     make_public('LatLng', LatLng);
-    
+
+
+
+
+    function MapControl(map, opt_options){
+        
+    }
     
     
     
@@ -356,15 +612,6 @@ minimize the code.
             el.removeChild(el.firstChild)
         }
     }
-    
-    
-    
-    if(!Math.pi){ // define a Math.pi function
-        Math.pi = function(){
-            return 3.14159265359;
-        }
-    }
-    
     
     
     
