41707caa2b20084aedda6f338895eaaf32adc218
Jeremy Geerdes <jrgeerdes@gmail.com>
2015-08-06 22:55:41 -0500

    Added license.txt, licensing preamble to files.

--- js/osm_webgl.js
+++ js/osm_webgl.js
@@ -7,6 +7,21 @@ the Google Maps API, except where it makes sense to do something better. We're
 also aiming to run it through Google's Closure Compiler so we can optimize and
 minimize the code.
 
+Copyright (C) 2014-2015  Jeremy R. Geerdes
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ******************************************************************************/
 
 
